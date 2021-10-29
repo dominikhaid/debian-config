ebb45a90c7625c1e854270b9c93ae103e9c60b0d
dominikhaid <46886220+dominikhaid@users.noreply.github.com>
2021-10-30 01:53:31 +0200

    changed to light hteme for better contrast

--- user-config/.config/rofi/bin/rofi-configmenu.sh
+++ user-config/.config/rofi/bin/rofi-configmenu.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-#                    ___ __                                     
+#                    ___ __
 # .----.-----.-----.'  _|__|.-----. .--------.-----.-----.--.--.
 # |  __|  _  |     |   _|  ||  _  | |        |  -__|     |  |  |
 # |____|_____|__|__|__| |__||___  | |__|__|__|_____|__|__|_____|
-#                           |_____| 
+#                           |_____|
 
 # Author: Dilip Chauhan
 # Github: https://github/TechnicalDC
 
 # Replace nvim with the editor of your choice
 
-MYEDITOR="kitty -e vim"
+MYEDITOR="kitty -e nvim"
 
 declare -A options
 options[aliases]="$HOME/.aliases"
@@ -31,14 +31,14 @@ options[zsh]="$HOME/.zshrc"
 
 # Piping the above array into dmenu.
 # We use "printf '%s\n'" to format the array one item to a line.
-choice=$(printf '%s\n' "${!options[@]}" | rofi -dmenu -no-show-icons -no-sidebar-mode -line 10  -p "config") 
+choice=$(printf '%s\n' "${!options[@]}" | rofi -dmenu -no-show-icons -no-sidebar-mode -line 10 -p "config")
 
 # What to do when/if we choose a file to edit.
 if [ "$choice" ]; then
-  conf=$(printf '%s\n' "${options["${choice}"]}")
+	conf=$(printf '%s\n' "${options["${choice}"]}")
 	$MYEDITOR "$conf"
 
 # What to do if we just escape without choosing anything.
 else
-    echo "Program terminated." && exit 0
+	echo "Program terminated." && exit 0
 fi
