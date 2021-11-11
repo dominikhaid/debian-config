25f8f40bf4e8a4bfb4d16e14947c7507dc59f05e
dominikhaid <info@dominikhaid.de>
2021-11-11 07:10:36 +0100

    fixed script

--- user-config/.config/rofi/bin/github-repos.sh
+++ user-config/.config/rofi/bin/github-repos.sh
@@ -27,32 +27,32 @@ URL="git@github.com:$USER/"
 
 TOKEN="token"
 # Clone a repository into the current directory
-clone_repository(){
-  local repository=$1
-  if [ -z "$repository" ]; then
-    echo "ERROR: You need to enter the name of the repository you wish to clone."
-  else
-  #  git clone "$URL$repository"
-  echo "git clone $URL$repository" | xclip -sel clip 
-  fi
+clone_repository() {
+	local repository=$1
+	if [ -z "$repository" ]; then
+		echo "ERROR: You need to enter the name of the repository you wish to clone."
+	else
+		#  git clone "$URL$repository"
+		echo "gh repo clone $repository" | xclip -sel clip
+		kitty -e gh repo clone $repository $HOME/Dokumente/$repository
+	fi
 }
 
 # Get all the repositories for the user with curl and GitHub API and filter only
 # the repository name from the output with sed substitution
-all_my_repositories_short_name(){
-  curl -H "Authorization: token $TOKEN" "https://api.github.com/search/repositories?q=user:$USER" | grep -o 'git@[^"]*' |\
-    sed "s/git@github.com:$USER\///g"
+all_my_repositories_short_name() {
+	gh repo list | awk '{ print $1 }'
 }
 
 # Rofi dmenu
-rofi_dmenu(){
-  rofi -dmenu -matching fuzzy -no-custom -p "Select a repository > "\
-    -location 0 -bg "#F8F8FF" -fg "#000000" -hlbg "#ECECEC" -hlfg "#0366d6"
+rofi_dmenu() {
+	rofi -dmenu -matching fuzzy -no-custom -p "Select a repository > " \
+		-location 0 -bg "#F8F8FF" -fg "#000000" -hlbg "#ECECEC" -hlfg "#0366d6"
 }
 
-main(){
-  repository=$(all_my_repositories_short_name | rofi_dmenu )
-  clone_repository "$repository"
+main() {
+	repository=$(all_my_repositories_short_name | rofi_dmenu)
+	clone_repository "$repository"
 }
 
 main
