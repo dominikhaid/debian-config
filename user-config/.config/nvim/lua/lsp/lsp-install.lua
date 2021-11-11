6f5f32973e5ebce149ffbdb3cb3c2221a56402d1
dominikhaid <info@dominikhaid.de>
2021-11-11 06:55:30 +0100

    replaced lspinstall with lsp-installer
    replace lspinstal
    
    fixup

--- user-config/.config/nvim/lua/lsp/lsp-install.lua
+++ user-config/.config/nvim/lua/lsp/lsp-install.lua
@@ -1,30 +1,37 @@
 local function setup_servers()
-  require "lspinstall".setup()
-  local servers = require "lspinstall".installed_servers()
+  local lsp_installer_servers = require "nvim-lsp-installer.servers"
+  local lsp_installer = require("nvim-lsp-installer")
 
-  for _, server in pairs(servers) do
-    require "lspconfig"[server].setup {
-      handlers = {
-        ["textDocument/publishDiagnostics"] = vim.lsp.with(
-          vim.lsp.diagnostic.on_publish_diagnostics,
-          {
-            virtual_text = true,
-            signs = true,
-            update_in_insert = false
-          }
-        )
+  -- Register a handler that will be called for all installed servers.
+  -- Alternatively, you may also register handlers on specific server instances instead (see example below).
+  lsp_installer.on_server_ready(
+    function(server)
+      local opts = {
+        handlers = {
+          ["textDocument/publishDiagnostics"] = vim.lsp.with(
+            vim.lsp.diagnostic.on_publish_diagnostics,
+            {
+              virtual_text = true,
+              signs = true,
+              update_in_insert = false
+            }
+          )
+        }
       }
-    }
-  end
+
+      -- (optional) Customize the options passed to the server
+      -- if server.name == "tsserver" then
+      --     opts.root_dir = function() ... end
+      -- end
+
+      -- This setup() function is exactly the same as lspconfig's setup function.
+      -- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/ADVANCED_README.md
+      server:setup(opts)
+    end
+  )
 end
 
 setup_servers()
-
--- Automatically reload after `:LspInstall <server>` so we don't have to restart neovim
---require "lspinstall".post_install_hook = function()
---setup_servers() -- reload installed servers
---vim.cmd("bufdo e") -- this triggers the FileType autocmd that starts the server
---end
 
 local signs = {Error = " ", Warning = " ", Hint = " ", Information = " "}
 
--- user-config/.config/nvim/lua/lsp/lsp-nav.lua
+++ user-config/.config/nvim/lua/lsp/lsp-nav.lua
@@ -1,25 +1,41 @@
-require "navigator".setup(
-  {
-    debug = false,
-    code_action_icon = " ",
-    width = 0.75,
-    height = 0.3,
-    preview_height = 0.35,
-    lspinstall = true,
-    border = {"╭", "─", "╮", "│", "╯", "─", "╰", "│"},
-    on_attach = function(client, bufnr)
-    end,
-    default_mapping = true,
-    keymaps = {{key = "gs", func = "signature_help()"}},
-    treesitter_analysis = true,
-    code_action_prompt = {enable = true, sign = true, sign_priority = 40, virtual_text = true},
-    icons = {
-      code_action_icon = " ",
-      diagnostic_head = "🐛",
-      diagnostic_head_severity_1 = "🈲"
-    },
-    lsp = {
-      format_on_save = true
-    }
+require'navigator'.setup({
+  debug = false, -- log output, set to true and log path: ~/.local/share/nvim/gh.log
+  width = 0.75,  
+  height = 0.3, -- max list window height, 0.3 by default
+  preview_height = 0.35, -- max height of preview windows
+  border = {"╭", "─", "╮", "│", "╯", "─", "╰", "│"}, 
+  on_attach = function(client, bufnr)
+  end,
+  default_mapping = true,  -- set to false if you will remap every key
+  keymaps = {{key = "gK", func = "declaration()"}}, -- a list of key maps
+  treesitter_analysis = true, -- treesitter variable context
+  transparency = 50,   
+  icons = {
+    -- Code action
+    code_action_icon = "🏏",
+    -- Diagnostics
+    diagnostic_head = '🐛',
+    diagnostic_head_severity_1 = "🈲",
+    -- refer to lua/navigator.lua for more icons setups
+  },
+  lsp_installer = true, -- set to true if you would like use the lsp installed by williamboman/nvim-lsp-installer
+  lsp = {
+    code_action = {enable = true, sign = true, sign_priority = 40, virtual_text = true},
+    code_lens_action = {enable = true, sign = true, sign_priority = 40, virtual_text = true},
+    format_on_save = true, -- set to false to disasble lsp code format on save (if you are using prettier/efm/formater etc)
+    disable_format_cap = {"sqls", "sumneko_lua", "gopls"},  -- a list of lsp disable format capacity (e.g. if you using efm or vim-codeformat etc), empty {} by default
+    disable_lsp = {'pylsd', 'sqlls'}, -- a list of lsp server disabled for your project, e.g. denols and tsserver you may
+    -- only want to enable one lsp server
+    -- to disable all default config and use your own lsp setup set
+    -- disable_lsp = 'all'
+    -- Default {}
+    diagnostic_scroll_bar_sign = {'▃', '▆', '█'}, -- experimental:  diagnostic status in scroll bar area; set to nil to disable the diagnostic sign,
+    -- for other style, set to {'╍', 'ﮆ'} or {'-', '='}
+    diagnostic_virtual_text = true,  -- show virtual for diagnostic message
+    diagnostic_update_in_insert = false, -- update diagnostic message in insert mode
+    disply_diagnostic_qf = true, -- always show quickfix if there are diagnostic errors, set to false if you  want to
   }
-)
+})
+
+
+
