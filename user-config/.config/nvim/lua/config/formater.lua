3e92664d8cba9cc3a5f1854f2ccc832891e5d6e0
dominikhaid <info@dominikhaid.de>
2021-11-11 06:55:14 +0100

    temporaly turned off markdown

--- user-config/.config/nvim/lua/config/formater.lua
+++ user-config/.config/nvim/lua/config/formater.lua
@@ -42,16 +42,16 @@ require("formatter").setup(
           }
         end
       },
-      markdown = {
-        -- prettier
-        function()
-          return {
-            exe = "prettier",
-            args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote"},
-            stdin = true
-          }
-        end
-      },
+      --markdown = {
+        ---- prettier
+        --function()
+          --return {
+            --exe = "prettier",
+            --args = {"--stdin-filepath", vim.api.nvim_buf_get_name(0), "--single-quote"},
+            --stdin = true
+          --}
+        --end
+      --},
       javascriptreact = {
         -- prettier
         function()
